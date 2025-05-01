6e5ed2bb1d5841c569d3e05e800a0c68b6ec7064
Manish  Kumar <manishcutm2001@gmail.com>
2025-05-01 22:31:08 +0530

    tl project created

---
+++ TLProj01/src/main/java/com/mani/main/App.java
@@ -0,0 +1,11 @@
+package com.mani.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Radha Rani");
+    }
+}
